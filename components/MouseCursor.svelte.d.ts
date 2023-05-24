ef997b0c9dd45f8e30afd3d5d86169fe354e22d5
Sudarsan <89625816+beastnier@users.noreply.github.com>
2023-05-24 16:10:23 +0530

    Updates

---
+++ components/MouseCursor.svelte.d.ts
@@ -0,0 +1,23 @@
+/** @typedef {typeof __propDef.props}  MouseCursorProps */
+/** @typedef {typeof __propDef.events}  MouseCursorEvents */
+/** @typedef {typeof __propDef.slots}  MouseCursorSlots */
+export default class MouseCursor extends SvelteComponentTyped<{
+    [x: string]: never;
+}, {
+    [evt: string]: CustomEvent<any>;
+}, {}> {
+}
+export type MouseCursorProps = typeof __propDef.props;
+export type MouseCursorEvents = typeof __propDef.events;
+export type MouseCursorSlots = typeof __propDef.slots;
+import { SvelteComponentTyped } from "svelte";
+declare const __propDef: {
+    props: {
+        [x: string]: never;
+    };
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {};
+};
+export {};
---
+++ components/Navbar.svelte
@@ -0,0 +1,40 @@
+<script>
+    let scroll = 0;
+</script>
+
+<svelte:window bind:scrollY={ scroll } />
+
+<style>
+    svg:hover {
+        fill: #726eff;
+    }
+</style>
+
+<div class="text-white z-[1000] bg-color3 flex fixed flex-col w-full h-[8.2vh] text-lg items-center border-b">
+    <div class="flex w-full h-[8.2vh] text-lg items-center">
+        <h2 class="text-2xl h-[8.2vh] flex border-r p-4 basis-1/3">
+            Portfolio
+        </h2>
+        <div class="border-r h-[8.2vh] p-4 basis-1/3 flex flex-row items-center justify-center">
+            <a href="https://github.com/beastnier">
+                <svg height="32" fill="white" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="32" data-view-component="true">
+                    <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
+                </svg>
+            </a>
+        </div>
+        <div class="flex flex-row items-center justify-center h-[8.2vh] p-4 flex-grow">
+            <ul class="flex gap-9 flex-row items-center justify-center">
+                <li>
+                    <a href="/">
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a href="/projects">
+                        Projects
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
